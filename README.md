c7df9f164a98cde81cc3d8aaa785fe812fe63228
HoKangInfo <hokang.info@gmail.com>
2017-09-22 11:47:17 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # js-group
-JavaScript Type or category of detection
+a simple JavaScript Type or category of detection
+
+
+### history ###
+- 0.0.1    `isNumber`, `isInteger`, `isLength`, `isString`
+
+### install ###
+```
+npm install js-group
+```
+
+### how to use it ###
+```
+var isString = require('js-group').isString;
+isString(new String('this is a string'));
+// ==> true
+```
---
+++ js-group.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var isNumber = (value) => (typeof value == 'number' ||
+    (Object.prototype.toString.call(value) == '[object Number]'));
+
+var isInteger = (value) => (isNumber(value) && value % 1 == 0);
+
+var isLength = (value) => (isNumber(value) &&
+    value > -1 && value % 1 == 0 && value <= Number.MAX_SAFE_INTEGER);
+
+var isString = (value) => (typeof value == 'string') ||
+    (Object.prototype.toString.call(value) == '[object String]')
+
+
+module.exports = {
+    "isNumber": isNumber,
+    "isInteger": isInteger,
+    "isLength": isLength,
+    "isString": isString,
+};
